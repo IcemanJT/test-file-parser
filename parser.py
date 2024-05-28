45e063f5d062463185842fc4713271e8358f2123
Jeremi Torój <jeremi.toroj@icloud.com>
2024-05-28 16:11:38 +0200

    txt file parser done

--- parser.py
+++ parser.py
@@ -17,6 +17,29 @@ def process_file(file):
         raise ValueError('Invalid file format')
 
 
+def process_txt_file(file):
+    content = file.read().decode('utf-8')
+
+    num_rows = content.count('\n')
+    num_words = len(re.findall(r'\w+', content))
+    num_chars = len(content)
+    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', content)
+    #  I assume that phone number has 9/10 digits and can be separated by space or dash and may have country code
+    phone_pattern = r'(\+\d{1,3}\s?)?(\d{3}[\s-]?\d{3}[\s-]?\d{3,4})'
+    matched_numbers = re.findall(phone_pattern, content)
+    phone_numbers = [''.join(match) for match in matched_numbers]
+
+    summary = {
+        'num_rows': num_rows,
+        'num_words': num_words,
+        'num_chars': num_chars,
+        'emails': emails,
+        'phone_numbers': phone_numbers,
+    }
+
+    return summary
+
+
 def process_csv_file(file):
     df = pd.read_csv(file)
 
@@ -28,29 +51,6 @@ def process_csv_file(file):
     summary = {
         'columns': columns,
         'num_of_rows': num_of_rows,
-    }
-
-    return summary
-
-
-def process_txt_file(file):
-    content = file.read().decode('utf-8')
-
-    num_rows = content.count('\n')
-    num_words = len(re.findall(r'\w+', content))
-    num_chars = len(content)
-    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', content)
-    #  I assume that phone number has 9 digits and can be separated by space or dash and may have country code (+48)
-    phone_pattern = r'(\+48\s?)?(\d{3}[\s-]?\d{3}[\s-]?\d{3})'
-    matched_numbers = re.findall(phone_pattern, content)
-    phone_numbers = [''.join(match) for match in matched_numbers]
-
-    summary = {
-        'num_rows': num_rows,
-        'num_words': num_words,
-        'num_chars': num_chars,
-        'emails': emails,
-        'phone_numbers': phone_numbers,
     }
 
     return summary
